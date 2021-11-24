ea3798b1c3fef49f96e0182f3307ecb7ab162a4a
Jesse Monks <monksjesse@gmail.com>
2021-11-24 17:23:43 -0500

    improved logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,4 @@
-/* var question = document.querySelector("#question")
-var answer1 = document.querySelector("#answer1")
-var answer2 = document.querySelector("#answer2")
-var answer3 = document.querySelector("#answer3")
-var answer4 = document.querySelector("#answer4")
-var button = document.querySelector("#button")
-var confirm = document.querySelector("#confirmation")
-var pane1 = document.querySelector("#pane-1")
-var pane2 = document.querySelector("#pane-2")
-pane2.style.display = 'none'; */
-
-
-
-var questiontext = document.querySelector(".question")
-var answer1 = document.querySelector(".answer1")
-var answer2 = document.querySelector(".answer2")
-var answer3 = document.querySelector(".answer3")
-var answer4 = document.querySelector(".answer4")
+var questiontext = document.querySelector("#question")
 var pane1 = document.querySelector("#pane-1")
 var pane2 = document.querySelector("#pane-2")
 var timer = document.querySelector('#time')
@@ -23,28 +6,32 @@ var button = document.querySelector("#button")
 var confirm = document.querySelector(".confirmation")
 var gameover = document.querySelector(".game-over")
 var next = document.querySelector(".next")
+var answer = document.querySelectorAll(".answer")
 
 pane2.style.display = 'none';
-var secondsRemaining = 20;
-var i = 0; 
+var secondsRemaining = 60;
+var currentQuestion = 0;
 
 var questions = [
   {
     question: 'Which font-weight is not valid value?',
     answer: ['normal', 'bold', 'lightest', 'bolder'],
-    correctAnswer: 3,
+    correctAnswer: 2
   }, {
-    question: 'question 2',
-    answer: ['adsf', 'adsf', 'Answadsfer3', 'adsf'],
-    correctAnswer: 1,
+    question: 'Choose the correct HTML tag for the largest heading',
+    answer: ['head', 'H1', 'H6', 'Heading'],
+    correctAnswer: 1
   }, {
-    question: 'question 3',
-    answer: ['asdfasdfas', 'asdf', 'adsfasdfasdf', 'afd'],
-    correctAnswer: 1,
+    question: 'How can you make a list that lists the items with numbers?',
+    answer: ['ol', 'di', 'ul', 'List'],
+    correctAnswer: 0
   }, {
-    question: 'question 4',
-    answer: ['adff', 'adsfafd', 'Anadsfswer3', 'asdf'],
-    correctAnswer: 1,
+    question: 'What is the correct HTML for inserting an image?',
+    answer: ['Img alt="MyImage" image.gif /img',
+            'Image src="image.gif" alt="MyImage"',
+            'Img src="image.gif" alt="MyImage"',
+            'Img href="image.gif" alt="MyImage"'],
+    correctAnswer: 2
   }];
 
 
@@ -53,31 +40,19 @@ button.addEventListener("click", function () {
   quiz();
 });
 
-next.addEventListener("click", function() {
-  if (i < 3) {
-  increment();
-  quiz(); 
-} else {
-  pane2.style.display = 'none';
-  document.getElementById('game-over').textContent = 'Game Over';
-}
-});
-
 function increment() {
-  i = i + 1;
-  console.log(i)
-}
+  currentQuestion++;
+  quiz()
+};
 
 function setCounter(num) {
   timer.textContent = num;
-}
+};
 
 function setTime() {
-
   setCounter(secondsRemaining);
   var timerInterval = setInterval(function () {
     secondsRemaining--;
-
     setCounter(secondsRemaining);
     if (secondsRemaining === 0) {
       clearInterval(timerInterval);
@@ -86,42 +61,25 @@ function setTime() {
     }
 
   }, 1000);
-}
+};
 
 
-function quiz () {
+function quiz() {
   pane1.style.display = 'none';
   pane2.style.display = 'unset';
-  questiontext.textContent = questions[i].question;
-  answer1.textContent = questions[i].answer[0];
-  answer2.textContent = questions[i].answer[1];
-  answer3.textContent = questions[i].answer[2];
-  answer4.textContent = questions[i].answer[3];
-  
-  /* answer1.addEventListener("click", function () {
-    if (questions[0].correctAnswer) {
-      confirm.textContent = 'Correct!'
-    }else {
-        confirm.textContent = 'Wrong!'
+  questiontext.textContent = questions[currentQuestion].question; 
+  confirm.textContent = '';
+
+
+  for (var i = 0; i < questions[currentQuestion].answer.length; i++) {
+    document.querySelector(`#answer${i + 1}`).textContent = questions[currentQuestion].answer[i];
+    document.querySelector(`#answer${i + 1}`).addEventListener("click", function () {
+      if (this.textContent === questions[currentQuestion].answer[questions[currentQuestion].correctAnswer]) {
+        confirm.textContent = 'Correct!'
+        setTimeout(increment, 500);
+      } else {
+        secondsRemaining -= 10;
       }
-}) */
-}
-
-
-
-
-
-
-/* button.addEventListener("click", function () {
-  pane2.style.display = 'unset'
-  pane1.style.display = 'none';
-  question.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis blanditiis, laboriosam recusandae et sunt quaerat quos a delectus ipsum sapiente! Voluptatem rerum voluptatibus reiciendis qui unde inventore asperiores culpa ut!"
-  answer1.textContent = "test answer"
-  answer2.textContent = "test answer"
-  answer3.textContent = "test answer"
-  answer4.textContent = "test answer"
-}); */
-
-
-
-
+    })
+  }
+};
